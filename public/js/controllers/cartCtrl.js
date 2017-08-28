55ddb6e494b2c1a0b1ece8510c4c772af304d72d
Kaleigh Niemela <nkaleigh@users.noreply.github.com>
2017-08-28 17:48:18 -0600

    cart media queries

--- public/js/controllers/cartCtrl.js
+++ public/js/controllers/cartCtrl.js
@@ -3,7 +3,7 @@ angular.module('App').controller('cartCtrl', function($scope, service) {
     getCartItems = function() {
         console.log('cartCtrl');
         $scope.cartItems = service.getCartItems().then(function(response) {
-            console.log('cartCtrl:cartItems', response);            
+            console.log('cartCtrl:cartItems', response);
             $scope.cartItems = response;
         });
     };
